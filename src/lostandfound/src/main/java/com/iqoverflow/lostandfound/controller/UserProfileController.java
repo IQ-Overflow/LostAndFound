b58b2fa5d4e890d66ecc1e8a2217ef24817775e5
wuliaod <2228669175@qq.com>
2020-11-07 22:21:48 +0800

    测试

---
+++ src/lostandfound/src/main/java/com/iqoverflow/lostandfound/controller/UserProfileController.java
@@ -0,0 +1,26 @@
+package com.iqoverflow.lostandfound.controller;
+
+
+import com.iqoverflow.lostandfound.domain.User;
+import com.iqoverflow.lostandfound.service.UserProfileservice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+
+@RestController
+public class UserProfileController {
+    @Autowired
+    private UserProfileservice userProfileservice;
+@GetMapping("/getuserProfile")
+public User getUserProfile(Model model, HttpSession session ){
+    String openid = (String) session.getAttribute("openid");
+    User userProfile = userProfileservice.getUserProfile(openid);
+    model.addAttribute("userProfile",userProfile);
+     return userProfile;
+}
+
+}
