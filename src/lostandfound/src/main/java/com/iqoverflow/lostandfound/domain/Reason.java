ada249500a0305ea90af57fd30b9d1aa7ba0232c
Oldlynn5374 <475369943@qq.com>
2020-11-09 20:26:01 +0800

    修改Reason类

--- src/lostandfound/src/main/java/com/iqoverflow/lostandfound/domain/Reason.java
+++ src/lostandfound/src/main/java/com/iqoverflow/lostandfound/domain/Reason.java
@@ -1,40 +1,60 @@
 package com.iqoverflow.lostandfound.domain;
 
 public class Reason {
-    private boolean flag;
-    private String ID;
-    private String Message;
-
-    public boolean isFlag() {
-        return flag;
-    }
-
-    public void setFlag(boolean flag) {
-        this.flag = flag;
-    }
-
-    public String getID() {
-        return ID;
-    }
-
-    public void setID(String ID) {
-        this.ID = ID;
-    }
-
-    public String getMessage() {
-        return Message;
-    }
-
-    public void setMessage(String message) {
-        Message = message;
-    }
+    private String pID;//卡或物品的ID(stuID or oID)
+    private String fID;//申请人的uID
+    private String tID;//被申请人的uID
+    private String message;//申请理由
+    private Integer state;//申请状态.0：进行中  1：已同意  2：已拒绝
 
     @Override
     public String toString() {
         return "Reason{" +
-                "flag=" + flag +
-                ", ID='" + ID + '\'' +
-                ", Message='" + Message + '\'' +
+                "pID='" + pID + '\'' +
+                ", fID='" + fID + '\'' +
+                ", tID='" + tID + '\'' +
+                ", message='" + message + '\'' +
+                ", state=" + state +
                 '}';
+    }
+
+    public String getpID() {
+        return pID;
+    }
+
+    public void setpID(String pID) {
+        this.pID = pID;
+    }
+
+    public String getfID() {
+        return fID;
+    }
+
+    public void setfID(String fID) {
+        this.fID = fID;
+    }
+
+    public String gettID() {
+        return tID;
+    }
+
+    public void settID(String tID) {
+        this.tID = tID;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
     }
 }
