2e7107c83683d9055e334de3efe0d75a19983651
Oldlynn5374 <475369943@qq.com>
2020-11-18 17:11:58 +0800

    增加发布卡时返回消息

--- src/lostandfound/src/main/java/com/iqoverflow/lostandfound/controller/CardController.java
+++ src/lostandfound/src/main/java/com/iqoverflow/lostandfound/controller/CardController.java
@@ -1,5 +1,7 @@
 package com.iqoverflow.lostandfound.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.iqoverflow.lostandfound.domain.Card;
 import com.iqoverflow.lostandfound.service.CardService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.sql.Timestamp;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -31,8 +34,11 @@ public class CardController {
 
     //发布学生卡
     @PostMapping("/postCard")
-    public Boolean postCard(@RequestBody Map<String,Object> info, HttpServletRequest request){
+    public String postCard(@RequestBody Map<String,Object> info, HttpServletRequest request) throws JsonProcessingException {
         HttpSession session = request.getSession();
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String,Object> resultMap = new HashMap();
+
 
         String stuID = (String)info.get("stuID");
         String college = (String)info.get("college");
@@ -40,6 +46,20 @@ public class CardController {
         String uID = (String) session.getAttribute("openid");
         Boolean flag = (Boolean)info.get("flag");
         Timestamp time = new Timestamp(System.currentTimeMillis());
+
+        if(stuID == null || college == null || stuName == null || flag == null){
+            resultMap.put("msg","请输入完整的信息！");
+            resultMap.put("flag",false);
+            return mapper.writeValueAsString(resultMap);
+        }
+
+        if(stuID.length() != 10){
+            resultMap.put("msg","请输入正确的学号！");
+            resultMap.put("flag",false);
+            return mapper.writeValueAsString(resultMap);
+        }
+
+        
 
         Card card = new Card();
         card.setStuID(stuID);
@@ -52,9 +72,15 @@ public class CardController {
         try{
             cardService.postCard(card);
         }catch (Exception e){
-            return false;
+            resultMap.put("flag",false);
+            resultMap.put("msg","该学生卡已被发布！");
+
+            return mapper.writeValueAsString(resultMap);
         }
-        return true;
+        resultMap.put("flag",true);
+        resultMap.put("msg","发布成功");
+
+        return mapper.writeValueAsString(resultMap);
     }
 
     //根据信息找卡
