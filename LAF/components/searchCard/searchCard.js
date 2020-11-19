33fc7e2f85ad1c2365a1714bc654f9622c1ca960
kaolalala <1921346727@qq.com>
2020-11-19 23:11:21 +0800

    开始接寻卡部分接口

--- LAF/components/searchCard/searchCard.js
+++ LAF/components/searchCard/searchCard.js
@@ -1,4 +1,5 @@
 // components/searchCard/searchCard.js
+import request from '../../utils/request'
 Component({
   /**
    * 组件的属性列表
@@ -13,6 +14,10 @@ Component({
   data: {
     index: 0,
     array: ['捡到', '丢失'],
+    type: true,
+    stuNum: '',
+    name: '',
+    college: ''
   },
 
   /**
@@ -20,8 +25,49 @@ Component({
    */
   methods: {
     bindPickerChange: function(e) {
+      if(e.detail.value == 0) {
+        this.setData({
+          type: true,
+          index: e.detail.value
+        })
+      } else {
+        this.setData({
+          type: false,
+          index: e.detail.value
+        })
+      }
+      console.log(this.data.type)
+    },
+    getStuNum(e) {
       this.setData({
-        index: e.detail.value
+        stuNum: e.detail.value
+      })
+    },
+    getName(e) {
+      this.setData({
+        name: e.detail.value
+      })
+    },
+    getCollege(e) {
+      this.setData({
+        college: e.detail.value
+      })
+    },
+    search() {
+      console.log(this.data.stuNum, this.data.name, this.data.college)
+      let _this = this;
+      request({
+        url: '/card/searchCard',
+        data: {
+          stuID: this.data.stuNum,
+          college: this.data.college,
+          stuName: this.data.name,
+          flag: this.data.type
+        },
+        method: 'POST'
+      })
+      .then(res => {
+        console.log(res.data)
       })
     },
   }
