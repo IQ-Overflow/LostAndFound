2fa3bf7238d83adeb71e6b730e8f01851692f670
kaolalala <1921346727@qq.com>
2020-11-25 01:15:54 +0800

    接了申请联系的接口，还有我发布的学生卡接口

--- LAF/components/cardItem/cardItem.js
+++ LAF/components/cardItem/cardItem.js
@@ -1,3 +1,5 @@
+import request from '../../utils/request'
+
 // components/cardItem/cardItem.js
 Component({
   /**
@@ -11,7 +13,20 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    state: [{
+      name: '进行中',
+      color: '#c59f40'
+    }, {
+      name: '已结束',
+      color: '#7cc540'
+    }, {
+      name: '已删除',
+      color: '#c55b40'
+    }],
+    mode: 'other',    //当前卡片是自己还是他人发布的
+    openid: '',
+    showApplyInput: false,
+    applyReason: ''
   },
 
   /**
@@ -19,8 +34,67 @@ Component({
    */
   methods: {
     askForContact() {
+      let _this = this;
       wx.showModal({
-        
+        content: '确定申请对方的联系方式吗？',
+        success() {
+          request({
+            url: '/reason/appliesForContact',
+            method: 'POST',
+            data: {
+              pID: _this.data.cardMsg.stuID,
+              tID: _this.data.cardMsg.poster.uID,
+              message: _this.data.applyReason
+            }
+          })
+          .then(res => {
+            console.log(res)
+          })
+        }
+      })
+    },
+
+    deleteCard() {
+      wx.showModal({
+        content: '确定删除此信息吗？',
+        success() {
+
+        }
+      })
+    },
+
+    showApply() {
+      this.setData({
+        showApplyInput: true
+      })
+    },
+
+    hideApply() {
+      this.setData({
+        showApplyInput: false
+      })
+    },
+
+    getValue(e) {
+      this.setData({
+        applyReason: e.detail.value
+      })
+    }
+  },
+
+  attached() {
+    console.log('start')
+    let openid = wx.getStorageSync('openid')
+    this.setData({
+      openid
+    })
+    if(this.data.cardMsg.poster.uID == openid) {
+      this.setData({
+        mode: 'self'
+      })
+    } else {
+      this.setData({
+        mode: 'other'
       })
     }
   }
--- LAF/utils/request.js
+++ LAF/utils/request.js
@@ -1,10 +1,43 @@
 // import login from '../utils/login'
 
-export default function request(option) {
+export default request
+async function request(option) {
   let cookie = wx.getStorageSync("cookie")
-  console.log(cookie)
   if(!cookie && option.url.indexOf('/wxLogin') == -1) {
     //非登录接口
+    await login(option);
+    console.log(123)
+  }
+  return realRequest(cookie, option);
+}
+
+async function realRequest(cookie, option) {
+  let p = new Promise((resolve, reject) => {
+    wx.request({
+      url: "http://106.52.47.25:8080" + option.url,
+      data: option.data || {},
+      header: { 
+        "Content-Type": "application/json",
+        'Cookie': cookie
+      },
+      method: option.method || 'GET',
+      success: async (res)=>{
+        if(res.data.msg == '请重新登录') {
+          wx.removeStorageSync('cookie');
+          // console.log(option)
+          return await request(option);
+        }
+        resolve(res)
+      },
+      fail: reject,
+    })
+  })
+  return p;
+}
+
+
+async function login(option) {
+  await new Promise((resolve, reject) => {
     wx.getSetting({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
@@ -20,13 +53,14 @@ export default function request(option) {
                     method: 'POST',
                     data: userInfo
                   })
-                  .then(res => {
+                  .then(async res => {
                     if(res.data.code == 200) {
-                      wx.setStorageSync('cookie', res.cookies[0]);
+                      if(res.cookies.length > 0) wx.setStorageSync('cookie', res.cookies[0]);
                       wx.setStorageSync('userInfo', JSON.stringify(userInfo))
                       wx.setStorageSync('openid', res.data.openid)
-                      request(res.cookies[0], option);
+                      await realRequest(res.cookies[0], option);
                     }
+                    resolve();
                   })
                 }
               })
@@ -36,29 +70,9 @@ export default function request(option) {
           wx.navigateTo({
             url: '/pages/login/login'
           })
+          resolve()
         }
-        return;
       }
     })
-  }
-  return realRequest(cookie, option);
-}
-
-function realRequest(cookie, option) {
-  let p = new Promise((resolve, reject) => {
-    wx.request({
-      url: "http://106.52.47.25:8080" + option.url,
-      data: option.data || {},
-      header: { 
-        "Content-Type": "application/json",
-        'Cookie': cookie
-      },
-      method: option.method || 'GET',
-      success: async (res)=>{
-        resolve(res)
-      },
-      fail: reject,
-    })
   })
-  return p;
-}
+} 
